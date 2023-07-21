b92132cb29f3afbf8760292e498ded5cd76fe25c
elisabeth-h7 <62919487+elisabeth-h7@users.noreply.github.com>
2023-07-20 19:16:51 -0700

    Update App.test.js

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+import userEvent from '@testing-library/user-event';
+
+test('check checkbox functionality', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  userEvent.click(screen.getByTestId("checkbox"));
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+  //screen.debug();
+});
+
+test('tests button working for text input', () => {
+  render(<App/>);
+
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  
 });
